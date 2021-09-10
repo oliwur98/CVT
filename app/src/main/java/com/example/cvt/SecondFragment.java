09db89eb7104d3468923b09eca68d3a7a5a03313
oliver <oliver982009@hotmail.com>
2021-09-10 12:22:04 +0200

    oliwur

--- app/src/main/java/com/example/cvt/SecondFragment.java
+++ app/src/main/java/com/example/cvt/SecondFragment.java
@@ -1,5 +1,9 @@
 package com.example.cvt;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,18 +15,29 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.cvt.databinding.FragmentSecondBinding;
 
+
 public class SecondFragment extends Fragment {
 
+    EditText textInputEditTextFirstName, textInputEditTextLastName, textInputEditTextEmail, textInputEditTextPassword, textInputEditTextPasswordCheck;
     private FragmentSecondBinding binding;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
+
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+
+        binding= FragmentSecondBinding.inflate(inflater, container, false);
+        textInputEditTextFirstName = findViewById(R.id.firstName);
+        textInputEditTextLastName = findViewById(R.id.lastName);
+        textInputEditTextEmail = findViewById(R.id.registerEmail);
+        textInputEditTextPassword = findViewById(R.id.password);
+        textInputEditTextPasswordCheck = findViewById(R.id.passwordConfirm);
         return binding.getRoot();
+
 
     }
 
@@ -36,6 +51,15 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+        binding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            String firstName, lastName, email, password, passwordCheck;
+
+
+            }
+        });
+
     }
 
     @Override
